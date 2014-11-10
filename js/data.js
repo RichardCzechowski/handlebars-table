86e957ccbfe6b94c11104289c311e2da6aa024a8
Richard Tanner <dicktanner@gmail.com>
2014-11-10 10:19:03 -0800

    Add HTML Boilerplate

---
+++ js/data.js
@@ -0,0 +1,14 @@
+(function(){
+var context= {
+"countries":[
+  {name: "Belarus", code: "BY", pop: 9685000, eng: false},
+  {name: "Belize", code "BZ", pop: 314522, eng: true},
+  {name: "Canada", code: "CA", pop: 33679000, eng: true},
+  {name: "Cocos [Keeling] Islands", code: "CC", pop: 628, eng: true},
+  {name: "Democratic Republic of the Congo", code: "CD", pop: 70916439, eng: false},
+  {name: "Central African Republic", code: "CF", pop: 4844927, eng: false},
+  {name: "Republic of the Congo", code: "CG", pop: 3039126, eng: false},
+  {name: "Switzerland", code: "CH", pop: 7581000, eng: false},
+]
+
+});
