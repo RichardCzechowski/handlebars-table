efe2e9c9097a28403130f8a43cbcd0b4b9144367
Richard Tanner <dicktanner@gmail.com>
2014-11-10 11:20:53 -0800

    Bootstrap and Handlebars work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+(function(){
+  Handlebars.registerHelper('boole', function(eng){
+    if (eng){
+      return new Handlebars.SafeString("<span class='glyphicon glyphicon-ok'></span>")
+    }
+    else{
+      return new Handlebars.SafeString("<span class='glyphicon glyphicon-remove'></span>")
+    }
+  })
 
+  Handlebars.registerHelper('number', function(num){
+    if(num>5000000){
+      return new Handlebars.SafeString("class='text-danger'")
+    }
+    else  if(num>100000){
+      return new Handlebars.SafeString("class='text-success'")
+    }
+    else{
+      return
+    }
+
+  })
+
+})();
